ae153decfa92cd8edbc19c152f0af33486bc2d11
Romulo Sousa <romulosousa27@hotmail.com>
2020-10-15 23:27:23 -0300

    Model de Orfanatos

---
+++ nlw-3-happy/api/src/models/Orphanages.ts
@@ -0,0 +1,27 @@
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('orphanages')
+export default class Orphanages {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    latitude: string;
+
+    @Column()
+    longitude: string;
+
+    @Column()
+    about: string;
+    @Column()
+    instructions: string;
+
+    @Column()
+    opening_hours: string;
+
+    @Column()
+    open_on_weekends: boolean;
+}
