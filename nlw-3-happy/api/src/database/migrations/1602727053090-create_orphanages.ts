65347d2879119d0f8d0f54ddf83780ee11f6f003
Romulo Sousa <romulosousa27@hotmail.com>
2020-10-15 23:27:57 -0300

    migration de orfanatos

---
+++ nlw-3-happy/api/src/database/migrations/1602727053090-create_orphanages.ts
@@ -0,0 +1,33 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1602727053090 implements MigrationInterface {
+
+    /**
+     * Resposavel por Criar a tabela.
+     * @param queryRunner
+     */
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "orphanages",
+            columns: [
+                {name: 'id', type: 'interger', unsigned: true, isGenerated: true, generationStrategy: "increment"},
+                {name: 'name', type: 'varchar'},
+                {name: 'latitude', type: 'decimal', scale: 10, precision: 2},
+                {name: 'lontitude', type: 'decimal', scale: 10, precision: 2},
+                {name: 'about', type: 'text'},
+                {name: 'instructions', type: 'text'},
+                {name: 'opening_hours', type: 'varchar'},
+                {name: 'open_on_weekends', type: 'boolean'}
+            ]
+        }));
+    }
+
+    /**
+     * Responsável por dá um Drop na tabela.
+     * @param queryRunner
+     */
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orphanages');
+    }
+
+}
