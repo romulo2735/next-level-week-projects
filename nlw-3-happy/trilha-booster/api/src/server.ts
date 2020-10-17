d1360522c6fdbd68516df8e8e434e0542495aceb
Romulo Sousa <romulosousa27@hotmail.com>
2020-10-17 14:32:55 -0300

    aplicando o cors. inserindo a tratativa de erros com o express inserindo o path de uploads de imagenss

--- nlw-3-happy/trilha-booster/api/src/server.ts
+++ nlw-3-happy/trilha-booster/api/src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+import "express-async-errors"
+
 import './database/connection';
 
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json);
 app.use(routes);
+
+// exportando as urls de acesso para as imagens.
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+// disparando exeções mais condensadas.
+app.use(errorHandler);
 
 app.listen(3333);
