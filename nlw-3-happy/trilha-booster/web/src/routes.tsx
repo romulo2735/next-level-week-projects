3f703b8605e0aeea753fde4016def717fc3fc9df
Romulo Sousa <romulosousa27@hotmail.com>
2020-10-21 22:38:13 -0300

    Rotas

--- nlw-3-happy/trilha-booster/web/src/routes.tsx
+++ nlw-3-happy/trilha-booster/web/src/routes.tsx
@@ -3,13 +3,18 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Landing from "./Pages/Landing";
 import OrphanagesMap from "./Pages/OrphanagesMap";
+import Orphanage from "./Pages/Orphanage";
+import CreateOrphanage from "./Pages/CreateOrphanage";
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Landing} />
-                <Route path="/orphanages" component={OrphanagesMap} />
+                <Route path="/" exact component={Landing}/>
+                <Route path="/orphanages/map" component={OrphanagesMap} />
+
+                <Route path="/orphanage/create" component={CreateOrphanage} />
+                <Route path="/orphanage/:id" component={Orphanage} />
             </Switch>
         </BrowserRouter>
     );
